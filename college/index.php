a17e29edf1c0b36c0872e0c71f99de73aec86933
RadioLogic <nrc1mr.e3@gmail.com>
2017-05-04 10:23:25 -0400

    new file:   college/catalog2016-17.pdf 	new file:   college/classAdvice.docx 	new file:   college/collegiate.zip 	new file:   college/collegiateInfo.pdf 	new file:   college/courseList.pdf 	new file:   college/courseSchedule.pdf 	new file:   college/courseSchedule.xlsx 	new file:   college/creditRequirements.pdf 	new file:   college/index.php 	new file:   college/misc.pdf 	new file:   college/startingCollegiateForTheFirstTime.docx

---
+++ college/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<title>Collegiate High Documents</title>
+
+</head>
+<body>
+<main>
+<h1>Collegiate files for Beginners</h1>
+<br><br>
+<p>Here, I'll keep the majority of files with instructions for college</p>
+<br>
+<p>you can either download seperate files, or download the zip file with all of the items and contents</p>
+<br>
+<div style="border:thin solid #000;">
+<p><a href="collegiate.zip" target="_blank">collegiate.zip</a> | this file holds all of the files in this list</p>
+<p><a href="catalog2016-17.pdf" target="_blank">College Catalog</a> | Use this to find info on a college course here</p>
+<p><a href="courseList.pdf" target="_blank">courseList</a> | holds the shortened list of college classes and some info</p>
+<p><a href="creditRequirements.pdf" target="_blank">Credit Requirements</a> | The list of required credits for high school and college (!very important!)</p>
+<p><a href="courseSchedule.pdf" target="_blank">My schedule</a> | this is my schedule that you can use as an example for your classes</p>
+<p><a href="classAdvice.docx" target="_blank">Class Advice</a> | advice on classes from Nick Cottrell</p>
+<p><a href="misc.pdf">Misc</a> | will contain mostly extra stuff. Not really important</p>
+</div>
+</main>
+</body>
+</html>
